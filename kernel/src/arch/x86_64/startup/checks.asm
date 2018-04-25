54cb1f41f7ed7430f0a0ac62b93b89c62f053184
mintsuki <xenos1@protonmail.com>
2018-04-26 00:22:28 +0200

    Starting to work on higher half

--- kernel/src/arch/x86_64/startup/checks.asm
+++ kernel/src/arch/x86_64/startup/checks.asm
@@ -3,6 +3,13 @@ global check_long_mode
 
 extern textmodeprint
 
+%define kernel_phys_offset 0xffffffffc0000000
+
+section .data
+
+calls:
+    .textmodeprint      dq textmodeprint - kernel_phys_offset
+
 section .text
 bits 32
 check_cpuid:
@@ -26,15 +33,18 @@ check_cpuid:
     popfd
 
     cmp eax, ecx
-    je .no_cpuid
+    db 0x75, 0x0b   ; jne
+    add esp, 4
+    mov dword [esp], .no_cpuid - kernel_phys_offset
+    ret
     ret
 .no_cpuid:
-    mov esi, .msg
-    call textmodeprint
+    mov esi, .msg - kernel_phys_offset
+    call [(calls.textmodeprint) - kernel_phys_offset]
 .halt:
     cli
     hlt
-    jmp .halt
+    jmp .halt - kernel_phys_offset
 
 .msg db "CPUID not supported", 0
 
@@ -42,18 +52,24 @@ check_long_mode:
     mov eax, 0x80000000
     cpuid
     cmp eax, 0x80000001
-    jb .no_long_mode
+    db 0x73, 0x0b   ; jnb
+    add esp, 4
+    mov dword [esp], .no_long_mode - kernel_phys_offset
+    ret
 
     mov eax, 0x80000001
     cpuid
     test edx, 1 << 29 ; Check if the LM bit is set in the D register
-    jz .no_long_mode
+    db 0x75, 0x0b   ; jnz
+    add esp, 4
+    mov dword [esp], .no_long_mode - kernel_phys_offset
+    ret
     ret
 .no_long_mode:
-    mov esi, .no_lm_msg
-    call textmodeprint
+    mov esi, .no_lm_msg - kernel_phys_offset
+    call [(calls.textmodeprint) - kernel_phys_offset]
 .halt:
     cli
     hlt
-    jmp .halt
+    jmp .halt - kernel_phys_offset
 .no_lm_msg db "Long mode not available, system halted", 0
